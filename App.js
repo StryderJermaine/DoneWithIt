3de2e88f89477274155ce9e79abdb0e7380e1860
Raphael Yaadar <yaadar.5913211@gmail.com>
2023-09-16 21:25:28 +0000

    Added AppPicker

--- App.js
+++ App.js
@@ -11,15 +11,29 @@ import ListItem from './app/components/ListItem';
 import AccountScreen from './app/screens/AccountScreen';
 import ListingsScreen from './app/screens/ListingsScreen';
 import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
+import { useState } from 'react';
 
-
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+]
 
 export default function App() {
 
   console.log(Dimensions.get('screen'));
 
+  const [category, setCategory] = useState(); 
+
   return (
     <Screen>
+      <AppPicker
+         selectedItem={category}
+         onSelectItem={ item => setCategory(item)}
+         icon="apps"
+         placeholder="Category" 
+         items={categories} />
        <AppTextInput 
           placeholder='First name'
           icon='email' />
